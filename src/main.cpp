c158a060171cdb4adacfb35a508342402714865e
arisKoutsou <gkoutsourid@hotmail.com>
2017-12-25 19:42:15 +0200

    inserted a main...

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 25, 2017
+ *      Author: aris
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hey Lionto!!!" << endl;
+	return 0;
+}
