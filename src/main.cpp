55f06ba9e6d5b91a9ca3be557894ad9ac0201c48
arisKoutsou <gkoutsourid@hotmail.com>
2017-12-25 20:02:18 +0200

    inserted main

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 25, 2017
+ *      Author: aris
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "heuuu" << endl;
+	return 0;
+}
+
+
