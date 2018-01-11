60274ff73d7fd0cf71833f00c3792f3a6e6f4140
arisKoutsou <gkoutsourid@hotmail.com>
2018-01-11 21:23:05 +0200

    added more test scenarios, buy().

--- src/tests/heroOnGridTest.cpp
+++ src/tests/heroOnGridTest.cpp
@@ -73,33 +73,41 @@ int main() {
 	Market* market1 = new Market(10);	// maxCapacity of market1 is 10.
 	grid.addMarket(1, 3, market1);		// Add a market at the 2nd row, 4th column.
 
+	Item* sword = new Weapon("Excalibur");
+	market1->addItem(sword);		// Add an item to the Market.
+
 	// Νow add a Hero on the grid.
 	Hero* warrior = new Warrior(
 		&grid,			// This warrior lives on the previously made grid.
-		2, 0,			// Put him on the 1st row, 3rd column.
+		1, 3,			// Put him on the 1st row, 3rd column.
 		"Tryndamere"	// Name.
 		// Rest of the arguments to constructor default...
 	);
 	
-	grid.print();
+	grid.displayMap();
+
+	warrior->checkInventory();
+	warrior->buy("Excalibur");
+	warrior->checkInventory();
+
 	try {
-	  warrior->move(Hero::DOWN);
+	  warrior->move(Hero::LEFT);
 	}
 	catch (HeroMoveException& e) {
 	  cout << e.what() << endl;
 	}
 
-	grid.print();
-	
-	Item* sword = new Weapon("Excalibur");
+	grid.displayMap();
 
+	/*
 	warrior->getInventory().addItem(sword);
 
 	warrior->printStats();
 	
-	warrior->checkInventory();
+
 
 	warrior->printStats();
+	*/
 	
 	delete[] randomTileInfo;
 
