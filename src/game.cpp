a70b3d55af7336a75f76f643a4247fac784738e0
arisKoutsou <gkoutsourid@hotmail.com>
2018-02-12 11:57:17 +0200

    BUG!!! : can't create map twice and play. 2nd time game exits at start.

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,27 @@
 #include "game_utils.h"
+#include "menu/MainMenu.h"
 
 int main(int argc, char* argv[]) {
-  run(argc, argv);
-  return EXIT_SUCCESS;
+
+	MainMenu mainMenu;	// Create the default menu.
+	string userInput;
+
+	while (userInput != "Quit") {
+		mainMenu.welcome();	// Print silly title...
+
+		userInput = mainMenu.prompt();	// Get userInput.
+
+		if (userInput == "Play") {
+			run(argc, argv);
+		} else if (userInput == "GenerateMap") {
+			generateMap();
+		} else if (userInput == "Help") {
+//			mainMenu.help();
+		}
+	}
+
+	cout << endl
+	<< "Exiting game..." << endl;
+
+	return EXIT_SUCCESS;
 }
